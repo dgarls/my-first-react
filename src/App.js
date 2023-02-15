c7e2a5ae7282e4abb521b98def2e01fd0ce90409
dgarls <dgarls.business@gmail.com>
2023-02-15 16:59:56 -0700

    modified:   src/App.css 	modified:   src/App.js     	new file:   src/images/batman.jpg 	new file:   src/images/brian.png 	new file:   src/images/ye.jpg

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
-const todoItems = ['Swag', 'Bentley', 'CSGO']
-
-function printTodoList(items){
-    return items.map((item) => <h2>{item}</h2>)
-}
+import brian from './images/brian.png'
+import batman from './images/batman.jpg'
+import ye from './images/ye.jpg';
 
 function App(){
+    const images = [brian, batman, ye];
+    const [imageNum, setImageNum] = useState(0);
+
+    console.log(imageNum);
+
+    function displayImage() {
+        return <img src={images[Math.abs(imageNum % 3)]} alt='funny'></img>
+    }
+
+    function nextImage() {
+        setImageNum(prevNum => prevNum + 1);
+    }
+
+    function prevImage(){
+        setImageNum(prevNum => prevNum - 1)
+    }
+
     return (
         <>
-            <h1>
-                Hello World!
-            </h1>
-            <div>
-                These are my todos:
-                {printTodoList(todoItems)}
+            <h3>
+                Amazing Image Gallery
+            </h3>
+            <div class='wrapper'>
+                <div id='imageHolder'>
+                    { displayImage() }
+                </div>
             </div>
+            
+            <div class="buttonWrapper">
+                <button onClick={prevImage}>Back</button>
+                <button onClick={nextImage}>Next</button>
+            </div>
+            
         </>
     )
 }
