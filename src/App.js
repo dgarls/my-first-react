a3927df1551eb48588ab63b8e4197a5f0cc27c25
dgarls <dgarls.business@gmail.com>
2023-02-15 17:01:24 -0700

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import './App.css';
 import brian from './images/brian.png'
 import batman from './images/batman.jpg'
